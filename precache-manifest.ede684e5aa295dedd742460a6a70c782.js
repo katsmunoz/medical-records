240bf54747324eba012e48e414475bdc63f32d13
katsmunoz <katherine.smm@gmail.com>
2020-06-08 23:27:20 -0400

    Updates

---
+++ precache-manifest.ede684e5aa295dedd742460a6a70c782.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56cc779199678c6a313c2e215cf630e",
+    "url": "/medical-records/index.html"
+  },
+  {
+    "revision": "9c862e7f4690716e215e",
+    "url": "/medical-records/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c829433db7d0adf26367",
+    "url": "/medical-records/static/css/main.11aa05f6.chunk.css"
+  },
+  {
+    "revision": "9c862e7f4690716e215e",
+    "url": "/medical-records/static/js/2.fe3da22c.chunk.js"
+  },
+  {
+    "revision": "3cabab53dee4769bc4a8c63b1d32918c",
+    "url": "/medical-records/static/js/2.fe3da22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c829433db7d0adf26367",
+    "url": "/medical-records/static/js/main.127ef601.chunk.js"
+  },
+  {
+    "revision": "93d142f1b55b8d36e998",
+    "url": "/medical-records/static/js/runtime-main.7ec4e2af.js"
+  }
+]);
